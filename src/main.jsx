d9348f1818513abcd807efd2aefe7ba1d58baade
Abhishek Sunam <abhishek.adam1997@gmail.com>
2025-08-21 05:12:54 +0530

    strict mode comment undo

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,9 @@ import { store } from './app/store.js';
 import { Provider } from 'react-redux';
 
 createRoot(document.getElementById('root')).render(
-	// <StrictMode>
-	<Provider store={store}>
-		<App />
-	</Provider>
-	// </StrictMode>
+	<StrictMode>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</StrictMode>
 );
